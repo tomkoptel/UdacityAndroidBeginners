b0b090aa6e8721bc6095163dc6a1b80fe3d77d2f
Tom Koptel <tom.koptel@gmail.com>
2016-03-11 19:18:50 +0200

    BirthDay card

---
+++ app/src/main/java/com/tom/udacityandroidbegin/BirthDayPage.java
@@ -0,0 +1,13 @@
+package com.tom.udacityandroidbegin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BirthDayPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
